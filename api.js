ecf9a2c1bb34e7f8a7767beae443fcd4cc870808
northgoldsolutions <northgoldsolutions@gmail.com>
2026-09-03 03:59:20 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || '/api'
+const API_URL = 'https://answerbible-production.up.railway.app/api'
 
 async function request(path, options = {}) {
   const url = `${API_URL}${path}`
